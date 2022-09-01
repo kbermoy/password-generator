4f9da1bea30b71dc84048626c888b404a8f0f938
Kristine Bermoy <kbermoy02@gmail.com>
2022-08-31 19:31:12 -0700

    added prompts

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,20 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var lowerCase = prompt("Include lower case alphabets? Enter y/n");
+  var upperCase = prompt("Include upper case alphabets? Enter y/n");
+  var passwordLenght = prompt("Choose a # from 8-128 for password lenght");
+  var specialChar = prompt("Include special characters? Enter y/n or blank to skip");
+  
+  var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
