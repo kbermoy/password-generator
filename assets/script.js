19e1b01f79619232205b93ad752a5f330ba9099f
Kristine Bermoy <kbermoy02@gmail.com>
2022-09-01 16:41:57 -0700

    fixed functionality code

--- assets/script.js
+++ assets/script.js
@@ -6,30 +6,46 @@ var ABCoption = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var specialCharOption = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-//
-function writePassword() {
-  var lowerCase = window.prompt("Include lower case alphets? Enter y or n");
-  var indexLowerCase = Math.floor(Math.random() * abcOption.length);  
-  var randomLowerCase = abcOption[indexLowerCase]; 
-  
-  
-  var upperCase = window.prompt("Include upper case alphabets? Enter y or n");
-  var indexUpperCase = Math.floor(Math.random() * ABCoption.length);
-  var randomUpperCase = ABCoption[indexUpperCase];
+// create functionality containing the user's decisions, which we will then use the gathered values as an object
+function generatePassword() {
+  // Variable to store length pf password from user input
+  var length = parseInt(
+    window.prompt("How many characters would you like your password to contain?")
+  );
 
-  var specialCharOption = window.prompt("Include special characters? Enter y or n");
-  var indexSpecialChar = Math.floor(Math.random() * specialCharOption.length);
-  var randomSpecialChar = specialCharOption[indexSpecialChar];
+  var hasSpecialChar = confirm(
+    "Click OK to confrim including SPECIAL CHARACTERS."
+  );
+  var hasLowerCaseChar = confirm(
+    "Click OK to confirm including LOWER CASE CHARACTERS."
+  );
+  var hasUpperCaseChar = confirm(
+    "Click OK to confirm including UPPER CASE CHARACTERS."
+  );
 
-  var numericChar = window.prompt("Pick your password length: Chose a # from 8-128");
-
-  
-  var password = generatePassword(); 
+  if (hasSpecialChar == true) {
+    var indexSpecialChar = Math.floor(Math.random() * specialCharOption.length);
+    var randomSpecialChar = specialCharOption[indexSpecialChar];
+    window.alert("Password will include " + randomSpecialChar);
+  } else {
+  };
+  if (hasLowerCaseChar == true) {
+    var indexLowerCase = Math.floor(Math.random() * abcOption.length);
+    var randomLowerCase = abcOption[indexLowerCase];
+    window.alert("Password will include " + randomLowerCase);
+  } else {
+  };
+  if (hasUpperCaseChar == true) {
+    var indexUpperCase = Math.floor(Math.random() * ABCoption.length);
+    var randomUpperCase = ABCoption[indexUpperCase];
+    window.alert("Password will include " + randomUpperCase);
+  } else {
+  };
+};
+var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
 
-passwordText.value = password;  
-}
-
+passwordText.value = password; 
 
 // Event listener to generate button
 generateBtn.addEventListener("click", writePassword);
