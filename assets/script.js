6a8a94976f5fc364be2851baa2fc1f6d53c24a5d
Kristine Bermoy <kbermoy02@gmail.com>
2022-09-14 23:06:01 -0700

    this is the previous code before restarting using a notion notes'

--- assets/script.js
+++ assets/script.js
@@ -1,86 +1,88 @@
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Array of options for computer to pick from
-var abcOption = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var ABCoption = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"];
-var specialCharOption = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var length = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122", "123", "124", "125", "126", "127", "128"];
 
-// create functionality containing the user's decisions, which we will then use the gathered values as an object
-function generatePasswordOption() {
-  // Variable to store length pf password from user input
-  var length = (
-    window.prompt("How many characters would you like your password to contain? Enter a # from 0-9")
-  );
-console.log(length);
-console.log(typeof length);
-  // write conditional statement here that handles the results of the user input that is NaN [in your conditional statement make sure to alert the user and return null]
-  if (length === length) {
-    window.alert("Not Valid!")
-  } else {
-    " "
-  };
-  // write conditional statement here that handles a number less than 8
-  if (length < 8) {
-    return;
-  } else {
-    " "
-  };
-  // write conditional statement here that handles a number greater than 128
-  if (length > 128) {
-    return;
-  } else {
-    " "
-  };
-  
-  var hasSpecialChar = confirm(
-    "Click OK to confrim including SPECIAL CHARACTERS."
-  );
-  var hasLowerCaseChar = confirm(
-    "Click OK to confirm including LOWER CASE CHARACTERS."
-  );
-  var hasUpperCaseChar = confirm(
-    "Click OK to confirm including UPPER CASE CHARACTERS."
-  );
-  var hasNumericChar = confirm(
-    "Click OK to confirm including NUMERIC CHARACTERS"
-  );
+//Create the Window Prompts
+const hasLowerCase = confirm (
+  "Click OK to confirm including LOWER CASE CHARACTERS."
+);
+const hasNumericChar = confirm(
+  "Click OK to confirm including NUMERIC CHARACTERS."
+)
+const hasSpecialChar = confirm (
+  "Click OK to confirm including SPECIAL CHARACTERS."
+);
+const hasUpperCase = confirm(
+  "Click OK to confirm including UPPER CASE CHARACTERS."
+);
+const passwordLength = window.prompt(
+  "Enter a number betwee 8-128 for your password length."
+);
+
+//Put Promps into one object
+const promptsConfirm = [
+  {hasLowerCase},
+  {hasNumericChar},
+  {hasSpecialChar},
+  {hasUpperCase},
+  {passwordLength}
+
+  //password.innerText = generatePassword
+];
+console.log(promptsConfirm),
+
+/* DOM Elements
+const passwordEl = document.getElementById('password');
+const lengthtEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const specialsEl = document.getElementById('specials');
+const generateEl = document.getElementById('generate');
+*/
+
+/* const randomFun = {
+  lower: getRandomLower(), //(this it a key called lower)
+  upper: getRandomUpper(),  //(this is a key called upper)
+  number: getRandomNumber(),
+  special: getRandomSpecial()
 };
+*/
 
-  /* review code with TA
-  
-  if (hasSpecialChar == true) {
-    var indexSpecialChar = Math.floor(Math.random() * specialCharOption.length);
-    var randomSpecialChar = specialCharOption[indexSpecialChar];
-    window.alert("Password will include " + randomSpecialChar);
-  } else {
-  };
-  if (hasLowerCaseChar == true) {
-    var indexLowerCase = Math.floor(Math.random() * abcOption.length);
-    var randomLowerCase = abcOption[indexLowerCase];
-    window.alert("Password will include " + randomLowerCase);
-  } else {
-  };
-  if (hasUpperCaseChar == true) {
-    var indexUpperCase = Math.floor(Math.random() * ABCoption.length);
-    var randomUpperCase = ABCoption[indexUpperCase];
-    window.alert("Password will include " + randomUpperCase);
-  } else {
-  };
-  if (hasNumericChar == true) {
-    var indexNumericChar = Math.floor(Math.random() * numericChar.length);
-    var randomNumericChar = numericChar[indexNumericChar];
-    window.alert("Password will include " + randomNumericChar);
-  }
-}; */ 
+generateEl.addEventListener('click', () => {
+  const length = lengthtEl.value;
 
-var password = generatePasswordOption(); 
-var passwordText = document.querySelector("#password");
+  console.log(length);
+})
 
-passwordText.value = password; 
+// Create functions to get the random characters
+function getRandomLower () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
 
-// Event listener to generate button
-generateBtn.addEventListener("click", generatePasswordOption);
+function getRandomUpper () {
+  return String.fromCharCode(Math.floor(Math.random() * 26 ) + 65);
+}
 
+function getRandomNumber () {
+  const numbers = "1234567890";
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
 
+function getRandomSpecial() {
+  const specials = "!@#$%^&*(){}[]=<>/,.";
+  return specials[Math.floor(Math.random() * specials.length)];
+}
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
